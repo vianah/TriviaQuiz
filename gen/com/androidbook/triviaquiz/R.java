605b854d91139b922a1cbafd273c20d325fadd81
Ville Ahti <vianah@utu.fi>
2012-02-06 11:13:53 +0200

    same

--- gen/com/androidbook/triviaquiz/R.java
+++ gen/com/androidbook/triviaquiz/R.java
@@ -13,6 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int linearLayout1=0x7f050000;
+    }
     public static final class layout {
         public static final int game=0x7f030000;
         public static final int help=0x7f030001;
