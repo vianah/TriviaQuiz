a0b02c6ed5c16af5098e8a09d189e85466ae4357
Ville Ahti <vianah@utu.fi>
2012-02-09 20:20:08 +0200

    splash screen almost done

--- gen/com/androidbook/triviaquiz/R.java
+++ gen/com/androidbook/triviaquiz/R.java
@@ -10,16 +10,34 @@ package com.androidbook.triviaquiz;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background_color=0x7f040002;
+        public static final int title_text_color=0x7f040000;
+        public static final int version_text_color=0x7f040001;
+    }
+    public static final class dimen {
+        public static final int control_text_font_size=0x7f050001;
+        public static final int title_font_size=0x7f050000;
+        public static final int version_text_spacing=0x7f050002;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int quizicon=0x7f020000;
+        public static final int splash1=0x7f020001;
+        public static final int splash2=0x7f020002;
+        public static final int splash3=0x7f020003;
+        public static final int splash4=0x7f020004;
     }
     public static final class id {
-        public static final int tableLayout1=0x7f050001;
-        public static final int tableRow1=0x7f050002;
-        public static final int tableRow2=0x7f050003;
-        public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f050004;
-        public static final int textView3=0x7f050005;
+        public static final int TextViewBottomTitle=0x7f070008;
+        public static final int TextViewTopTitle=0x7f070000;
+        public static final int imageView1=0x7f070003;
+        public static final int imageView2=0x7f070004;
+        public static final int imageView3=0x7f070006;
+        public static final int imageView4=0x7f070007;
+        public static final int tableLayout1=0x7f070001;
+        public static final int tableRow1=0x7f070002;
+        public static final int tableRow2=0x7f070005;
+        public static final int textViewBottomVersion=0x7f070009;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -30,12 +48,15 @@ public final class R {
         public static final int splash=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int game=0x7f040005;
-        public static final int help=0x7f040001;
-        public static final int menu=0x7f040002;
-        public static final int scores=0x7f040006;
-        public static final int settings=0x7f040004;
-        public static final int splash=0x7f040003;
+        public static final int app_name=0x7f060000;
+        public static final int bottom_title=0x7f060008;
+        public static final int game=0x7f060005;
+        public static final int help=0x7f060001;
+        public static final int menu=0x7f060002;
+        public static final int scores=0x7f060006;
+        public static final int settings=0x7f060004;
+        public static final int splash=0x7f060003;
+        public static final int top_title=0x7f060007;
+        public static final int version_info=0x7f060009;
     }
 }
