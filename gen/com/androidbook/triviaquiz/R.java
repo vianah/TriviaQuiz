f58501a90e6df7827867711f3263d993ef93f782
Ville Ahti <vianah@utu.fi>
2012-02-09 21:11:33 +0200

    splash screen animations added

--- gen/com/androidbook/triviaquiz/R.java
+++ gen/com/androidbook/triviaquiz/R.java
@@ -8,17 +8,22 @@
 package com.androidbook.triviaquiz;
 
 public final class R {
+    public static final class anim {
+        public static final int custom_anim=0x7f040000;
+        public static final int fade_in=0x7f040001;
+        public static final int fade_in2=0x7f040002;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int background_color=0x7f040002;
-        public static final int title_text_color=0x7f040000;
-        public static final int version_text_color=0x7f040001;
+        public static final int background_color=0x7f050002;
+        public static final int title_text_color=0x7f050000;
+        public static final int version_text_color=0x7f050001;
     }
     public static final class dimen {
-        public static final int control_text_font_size=0x7f050001;
-        public static final int title_font_size=0x7f050000;
-        public static final int version_text_spacing=0x7f050002;
+        public static final int control_text_font_size=0x7f060001;
+        public static final int title_font_size=0x7f060000;
+        public static final int version_text_spacing=0x7f060002;
     }
     public static final class drawable {
         public static final int quizicon=0x7f020000;
@@ -28,16 +33,16 @@ public final class R {
         public static final int splash4=0x7f020004;
     }
     public static final class id {
-        public static final int TextViewBottomTitle=0x7f070008;
-        public static final int TextViewTopTitle=0x7f070000;
-        public static final int imageView1=0x7f070003;
-        public static final int imageView2=0x7f070004;
-        public static final int imageView3=0x7f070006;
-        public static final int imageView4=0x7f070007;
-        public static final int tableLayout1=0x7f070001;
-        public static final int tableRow1=0x7f070002;
-        public static final int tableRow2=0x7f070005;
-        public static final int textViewBottomVersion=0x7f070009;
+        public static final int TextViewBottomTitle=0x7f080008;
+        public static final int TextViewTopTitle=0x7f080000;
+        public static final int imageView1=0x7f080003;
+        public static final int imageView2=0x7f080004;
+        public static final int imageView3=0x7f080006;
+        public static final int imageView4=0x7f080007;
+        public static final int tableLayout1=0x7f080001;
+        public static final int tableRow1=0x7f080002;
+        public static final int tableRow2=0x7f080005;
+        public static final int textViewBottomVersion=0x7f080009;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -48,15 +53,15 @@ public final class R {
         public static final int splash=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int bottom_title=0x7f060008;
-        public static final int game=0x7f060005;
-        public static final int help=0x7f060001;
-        public static final int menu=0x7f060002;
-        public static final int scores=0x7f060006;
-        public static final int settings=0x7f060004;
-        public static final int splash=0x7f060003;
-        public static final int top_title=0x7f060007;
-        public static final int version_info=0x7f060009;
+        public static final int app_name=0x7f070000;
+        public static final int bottom_title=0x7f070008;
+        public static final int game=0x7f070005;
+        public static final int help=0x7f070001;
+        public static final int menu=0x7f070002;
+        public static final int scores=0x7f070006;
+        public static final int settings=0x7f070004;
+        public static final int splash=0x7f070003;
+        public static final int top_title=0x7f070007;
+        public static final int version_info=0x7f070009;
     }
 }
--- src/com/androidbook/triviaquiz/QuizSplashActivity.java
+++ src/com/androidbook/triviaquiz/QuizSplashActivity.java
@@ -2,6 +2,9 @@ package com.androidbook.triviaquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.*;
 
 public class QuizSplashActivity extends QuizActivity {
     /** Called when the activity is first created. */
@@ -9,5 +12,22 @@ public class QuizSplashActivity extends QuizActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        
+        TextView logo1 = (TextView) this.findViewById(R.id.TextViewTopTitle);
+        Animation fadeIn1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fadeIn1);
+        
+        TextView logo2 = (TextView) this.findViewById(R.id.TextViewBottomTitle);
+        Animation fadeIn2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        logo2.startAnimation(fadeIn2);
+       
     }
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		TextView logo1 = (TextView) this.findViewById(R.id.TextViewBottomTitle);
+		logo1.clearAnimation();
+	}
 }
