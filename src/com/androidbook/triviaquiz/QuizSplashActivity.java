a0301cc3a5f1a000565ae57313d0cf14f2094cd2
Ville Ahti <vianah@utu.fi>
2012-02-14 20:03:53 +0200

    splash screen ready

--- src/com/androidbook/triviaquiz/QuizSplashActivity.java
+++ src/com/androidbook/triviaquiz/QuizSplashActivity.java
@@ -1,9 +1,10 @@
 package com.androidbook.triviaquiz;
 
 import android.app.Activity;
+import android.content.*;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.animation.*;
+import android.view.animation.Animation.*;
 import android.widget.*;
 
 public class QuizSplashActivity extends QuizActivity {
@@ -12,7 +13,11 @@ public class QuizSplashActivity extends QuizActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        
+        startAnimating();
+    }
+    
+    private void startAnimating() {
+    	
         TextView logo1 = (TextView) this.findViewById(R.id.TextViewTopTitle);
         Animation fadeIn1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         logo1.startAnimation(fadeIn1);
@@ -20,6 +25,30 @@ public class QuizSplashActivity extends QuizActivity {
         TextView logo2 = (TextView) this.findViewById(R.id.TextViewBottomTitle);
         Animation fadeIn2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
         logo2.startAnimation(fadeIn2);
+        
+        Animation spinnin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController  controller = new LayoutAnimationController(spinnin);
+        controller.setOrder(2);
+        TableLayout table = (TableLayout) this.findViewById(R.id.tableLayout1);
+        for (int i = 0; i < table.getChildCount(); i++) {
+        	TableRow row = (TableRow) table.getChildAt(i);
+        	row.setLayoutAnimation(controller);
+		}
+        
+        fadeIn2.setAnimationListener(new AnimationListener() {
+        	
+			public void onAnimationEnd(Animation animation) {
+        		// TODO Auto-generated method stub
+        		startActivity(new Intent(QuizSplashActivity.this, 
+        				QuizMenuActivity.class));
+        		QuizSplashActivity.this.finish();
+        	}
+			public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationStart(Animation animation) {
+            }
+        });
        
     }
 
@@ -27,7 +56,24 @@ public class QuizSplashActivity extends QuizActivity {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
-		TextView logo1 = (TextView) this.findViewById(R.id.TextViewBottomTitle);
+		TextView logo1 = (TextView) this.findViewById(R.id.TextViewTopTitle);
 		logo1.clearAnimation();
+		
+		TextView logo2 = (TextView) this.findViewById(R.id.TextViewBottomTitle);
+		logo2.clearAnimation();
+		
+		TableLayout table = (TableLayout) this.findViewById(R.id.tableLayout1);
+		for (int i = 0; i < table.getChildCount(); i++) {
+			TableRow row = (TableRow) table.getChildAt(i);
+			row.clearAnimation();
+		}
 	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		startAnimating();
+	}
+	
 }
