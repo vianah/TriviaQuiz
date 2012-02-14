f67066189a13e7d4a15d3245988f1db43e500b45
Ville Ahti <vianah@utu.fi>
2012-02-14 22:12:48 +0200

    started implementing menu screen

--- gen/com/androidbook/triviaquiz/R.java
+++ gen/com/androidbook/triviaquiz/R.java
@@ -17,47 +17,67 @@ public final class R {
     }
     public static final class color {
         public static final int background_color=0x7f050002;
+        public static final int menu_color=0x7f050005;
+        public static final int menu_glow=0x7f050006;
+        public static final int title_color=0x7f050003;
+        public static final int title_glow=0x7f050004;
         public static final int title_text_color=0x7f050000;
         public static final int version_text_color=0x7f050001;
     }
     public static final class dimen {
         public static final int control_text_font_size=0x7f060001;
+        public static final int listview_item_size=0x7f060004;
+        public static final int menu_screen_title=0x7f060003;
         public static final int title_font_size=0x7f060000;
         public static final int version_text_spacing=0x7f060002;
     }
     public static final class drawable {
-        public static final int quizicon=0x7f020000;
-        public static final int splash1=0x7f020001;
-        public static final int splash2=0x7f020002;
-        public static final int splash3=0x7f020003;
-        public static final int splash4=0x7f020004;
+        public static final int bkgrnd=0x7f020000;
+        public static final int divider=0x7f020001;
+        public static final int half=0x7f020002;
+        public static final int quizicon=0x7f020003;
+        public static final int splash1=0x7f020004;
+        public static final int splash2=0x7f020005;
+        public static final int splash3=0x7f020006;
+        public static final int splash4=0x7f020007;
+        public static final int textured=0x7f020008;
     }
     public static final class id {
-        public static final int TextViewBottomTitle=0x7f080008;
-        public static final int TextViewTopTitle=0x7f080000;
-        public static final int imageView1=0x7f080003;
-        public static final int imageView2=0x7f080004;
-        public static final int imageView3=0x7f080006;
-        public static final int imageView4=0x7f080007;
-        public static final int tableLayout1=0x7f080001;
-        public static final int tableRow1=0x7f080002;
-        public static final int tableRow2=0x7f080005;
-        public static final int textViewBottomVersion=0x7f080009;
+        public static final int TextViewBottomTitle=0x7f08000c;
+        public static final int TextViewTopTitle=0x7f080007;
+        public static final int imageView1=0x7f080001;
+        public static final int imageView2=0x7f080003;
+        public static final int imageView3=0x7f080005;
+        public static final int imageView4=0x7f08000b;
+        public static final int listView_menu=0x7f080006;
+        public static final int relativeLayout1=0x7f080000;
+        public static final int relativeLayout2=0x7f080004;
+        public static final int tableLayout1=0x7f080008;
+        public static final int tableRow1=0x7f080009;
+        public static final int tableRow2=0x7f08000a;
+        public static final int textView1=0x7f080002;
+        public static final int textViewBottomVersion=0x7f08000d;
     }
     public static final class layout {
         public static final int game=0x7f030000;
         public static final int help=0x7f030001;
         public static final int menu=0x7f030002;
-        public static final int scores=0x7f030003;
-        public static final int settings=0x7f030004;
-        public static final int splash=0x7f030005;
+        public static final int menu_item=0x7f030003;
+        public static final int scores=0x7f030004;
+        public static final int settings=0x7f030005;
+        public static final int splash=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f070000;
         public static final int bottom_title=0x7f070008;
         public static final int game=0x7f070005;
         public static final int help=0x7f070001;
+        public static final int listview_item1=0x7f07000a;
+        public static final int listview_item2=0x7f07000b;
+        public static final int listview_item3=0x7f07000c;
+        public static final int listview_item4=0x7f07000d;
         public static final int menu=0x7f070002;
+        public static final int menu_title=0x7f07000e;
         public static final int scores=0x7f070006;
         public static final int settings=0x7f070004;
         public static final int splash=0x7f070003;
